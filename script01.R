d844d80a6c6c70b8a5082354bd8c1bf7e619fe5a
Fabio Oliveira <fabiobo@msn.com>
2021-11-28 13:03:02 -0300

    versão 3

--- script01.R
+++ script01.R
@@ -1,16 +1,12 @@
 # Data Wrangling R
-
 # Introdução ao pacote dplyr (https://dplyr.tidyverse.org/)
-
 # O pacote dplyr está contido no tidyverse
 # dplyr: contém muitas funções comuns na manipulação de dados
-
 # Se for a primeira vez que utiliza o tidyverse, instale-o
 
 install.packages("tidyverse")
 
 # Se já instalou para análises anteriores, basta carregar o pacote
-
 library("tidyverse")
 
 
